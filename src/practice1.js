88077bf50faec2c7642b7e7ccaed065045c558fc
wenwei.song <wenwei.song@rea-group.com>
2020-12-18 14:33:01 +0800

    finish practices.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = ({data, column})=> {
+  const result = [];
+  data.map(item => {
+    let member = {};
+    column.forEach(it => {
+      member[it.name] = item.shift();
+    })
+    result.push(member);
+  })
+  return result
+}
+
 export { parseData };
